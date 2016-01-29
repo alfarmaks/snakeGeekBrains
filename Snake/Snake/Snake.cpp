3aaeb6e390e7a073dbc4eeb9137042a28eb2bbfa
maksymka <bondarmaksymwork@gmail.com>
2016-01-29 19:25:40 +0200

    Lesson1

---
+++ Snake/Snake/Snake.cpp
@@ -0,0 +1,14 @@
+// Snake.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello, World!" << endl;
+	system("pause>>void"); //Stop program while user don't press any key. 
+	return 0;
+}
+
