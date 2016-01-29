a34e3cb2cc966db8289c07533981a301649f1105
maksymka <bondarmaksymwork@gmail.com>
2016-01-29 20:11:27 +0200

    Update our code

--- Snake/Snake/Snake.cpp
+++ Snake/Snake/Snake.cpp
@@ -4,20 +4,23 @@
 #include "stdafx.h"
 #include <Windows.h>
 #include <iostream>
+#include "Point.h"
 using namespace std;
-
-void Draw(int x, int y, char sym){
-	COORD pos = { x, y };//struct for coordinate our point(symbol)
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);//descriptor
-	SetConsoleCursorPosition(hConsole, pos);
-	cout << sym;
-}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int x = 5;
-	int y = x;
-	Draw(x, y, '#');
+	//Point number one
+	Point p1;
+	p1.x = 1;
+	p1.y = 1;
+	p1.symb = 'X';
+	p1.Draw();
+	//Point number two
+	Point* p2 = new Point();
+	p2->x = 0;
+	p2->y = 0;
+	p2->symb = '#';
+	p2->Draw();
 	system("pause>>void"); //Stop program while user don't press any key. 
 	return 0;
 }
