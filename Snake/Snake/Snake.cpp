f7e7f0efdc4b23bee9649e6e0385969dd04c3c37
maksymka <bondarmaksymwork@gmail.com>
2016-01-29 19:48:58 +0200

    Procedural programming approuch

--- Snake/Snake/Snake.cpp
+++ Snake/Snake/Snake.cpp
@@ -2,12 +2,22 @@
 //
 
 #include "stdafx.h"
+#include <Windows.h>
 #include <iostream>
 using namespace std;
 
+void Draw(int x, int y, char sym){
+	COORD pos = { x, y };//struct for coordinate our point(symbol)
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);//descriptor
+	SetConsoleCursorPosition(hConsole, pos);
+	cout << sym;
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cout << "Hello, World!" << endl;
+	int x = 5;
+	int y = x;
+	Draw(x, y, '#');
 	system("pause>>void"); //Stop program while user don't press any key. 
 	return 0;
 }
